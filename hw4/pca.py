b0777617259e4a3c75583b37a3b3fccf4ff900c3
陳代穎 <dying1020@gmail.com>
2018-04-29 13:38:53 +0800

    hw4: Add PCA shell script

---
+++ hw4/pca.py
@@ -0,0 +1,52 @@
+# coding: utf-8
+import numpy as np
+from skimage import io
+import os
+import sys
+
+IMAGE_FOLDER = sys.argv[1]
+output_path = sys.argv[2]
+height = width = 600
+channel = 3
+
+def ensure_dir(filepath):
+    directory = os.path.dirname(filepath)
+    if len(directory) > 0 and not os.path.exists(directory):
+        os.makedirs(directory)
+
+def reconstruction(image, eigen_faces, X_mean):
+    c = np.dot((image-X_mean), eigen_faces.T)
+    image_rec = np.dot(c, eigen_faces)
+    image_rec += X_mean
+    return image_rec
+
+def output_image(image):
+    image = image.reshape(height, width, channel)
+    image -= np.min(image)
+    image /= np.max(image)
+    image = (image*255).astype(np.uint8)
+    io.imsave(output_path, image)
+
+def main():
+    image_files = sorted(os.listdir(IMAGE_FOLDER))
+    image_paths = [os.path.join(IMAGE_FOLDER, file) for file in image_files]
+    X_train = []
+    for i in image_paths:
+        X_train.append(io.imread(i).flatten())
+    X_train = np.array(X_train)
+
+    # Mean of the faces
+    X_mean = np.mean(X_train, axis=0)
+    U, s, V = np.linalg.svd((X_train-X_mean).T, full_matrices=False)
+
+    eigen_faces = U.T[:4]
+    ensure_dir(output_path)
+    index = image_files.index(os.path.basename(output_path))
+    image = X_train[index]
+
+    # Reconstruction
+    image_rec = reconstruction(image, eigen_faces, X_mean)
+    output_image(image_rec)
+
+if __name__ == "__main__":
+   main() 
